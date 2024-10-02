ac459b3cc6c8026dbb87cb4172de0ebd38baa06e
Lance <85811392+Lance-Town@users.noreply.github.com>
2024-10-02 14:07:43 -0700

    front page prompts user to sign in or sign up for app

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,35 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
-export default function Index() {
-  return (
-    <View
-      style={{
+const Index: React.FC = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Welcome to the Barter App</Text>
+            <Link href={{ pathname: "./signIn" }}>
+                <Button title="Sign In" />
+            </Link>
+            <Link href="./signUp" style={styles.button}>
+                <Button title="Sign Up" />
+            </Link>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+        padding: 16,
+    },
+    title: {
+        fontSize: 24,
+        marginBottom: 16,
+    },
+    button: {
+        marginTop: 8,
+    },
+});
+
+export default Index;
