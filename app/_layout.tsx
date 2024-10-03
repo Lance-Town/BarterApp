a7600bd8f4384adeeb9fca53ade6b9e2e807d10c
Lance <85811392+Lance-Town@users.noreply.github.com>
2024-10-03 09:02:59 -0700

    remove headers from page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,9 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
     return (
         <Stack>
-            <Stack.Screen name="index" />
-            <Stack.Screen name="signIn" />
-            <Stack.Screen name="signUp" />
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="signIn" options={{ headerShown: false }} />
+            <Stack.Screen name="signUp" options={{ headerShown: false }} />
         </Stack>
     );
 }
