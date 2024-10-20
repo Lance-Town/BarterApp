e673a0dafcc07446d0041273d9f8dd5a48879524
Lance <85811392+Lance-Town@users.noreply.github.com>
2024-10-20 13:16:48 -0700

    change bottom navigation tab background color
    - change the background color to the #292929 common background color

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,7 +4,15 @@ import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
     return (
-        <Tabs screenOptions={{ headerShown: false }}>
+        <Tabs
+            screenOptions={{
+                headerShown: false,
+                tabBarStyle: {
+                    backgroundColor: "#292929",
+                    borderTopWidth: 0,
+                },
+            }}
+        >
             {/* <Tabs.Screen name="index" options={{ title: "Home" }} />
             <Tabs.Screen name="items" options={{ title: "Items" }} />
             <Tabs.Screen
