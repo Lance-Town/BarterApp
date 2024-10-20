de39ce2f769e2f00993521472c3f952b6434d078
Lance <85811392+Lance-Town@users.noreply.github.com>
2024-10-20 13:49:58 -0700

    add basic ui to items page

--- app/(tabs)/items.tsx
+++ app/(tabs)/items.tsx
@@ -1,12 +1,68 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, FlatList, SafeAreaView } from "react-native";
+import { Text, Layout, StyleService } from "@ui-kitten/components";
+import { getUserItems, Item } from "@/backend/api";
+import { useStyleSheet } from "@ui-kitten/components";
+import { useUser } from "@/hooks/UserContext";
 
-const ItemsScreen = () => {
+const ItemsScreen: React.FC = () => {
+    const { userId } = useUser(); // get the user id from the contex
+    const [items, setItems] = useState<Item[]>([]);
+    useEffect(() => {
+        if (!userId) {
+            return;
+        }
+
+        const fetchItems = async () => {
+            try {
+                const response = await getUserItems(userId);
+                setItems(response);
+            } catch (error) {
+                console.error("Failed to fetch items:", error);
+            }
+        };
+
+        fetchItems();
+    }, [userId]);
+
     return (
-        <View>
-            <Text>Items for Sale</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={items}
+                keyExtractor={(item) => item.item_id.toString()}
+                renderItem={({ item }) => (
+                    <Layout style={styles.itemContainer}>
+                        <Text style={styles.itemName}>{item.name}</Text>
+                        <Text style={styles.itemName}>{item.value}</Text>
+                        <Text style={styles.itemName}>
+                            {item.transfer_cost}
+                        </Text>
+                    </Layout>
+                )}
+            ></FlatList>
+        </SafeAreaView>
     );
 };
+
+const styles = StyleService.create({
+    container: {
+        flex: 1,
+        padding: 16,
+    },
+    itemContainer: {
+        padding: 16,
+        backgroundColor: "#f8f8f8",
+        borderBottomWidth: 1,
+        borderColor: "#ddd",
+    },
+    itemName: {
+        fontSize: 18,
+        fontWeight: "bold",
+    },
+    itemDetails: {
+        fontSize: 14,
+        color: "#555",
+    },
+});
 
 export default ItemsScreen;
